01cd327caf16dba7793880bf9d9d1a130602dfb4
Amr <nm@192.168.1.28>
2026-07-01 15:40:33 +0100

    refactor: adopt feature-first Flutter layout with repositories and composed routing+optimize some code lines

--- test/dashboard_provider_test.dart
+++ test/dashboard_provider_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gojocalories/features/dashboard/providers/dashboard_provider.dart';
+
 
 void main() {
   test('DashboardProvider initial state is correctly padded', () {
